f6fbc8722de1d620fe0cd9b94545cf1217719677
Russell Gordon <rgordon@lcs.on.ca>
2024-01-15 09:00:44 -0500

    Initial Commit

---
+++ ChartsTutorial/ChartsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsTutorialApp.swift
+//  ChartsTutorial
+//
+//  Created by Russell Gordon on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct ChartsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
